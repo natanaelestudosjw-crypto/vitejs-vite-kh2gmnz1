ffc095bff648e0145667d7c6f89ee0fa69624de3
natanaelestudosjw-crypto <natanael.estudos.jw@gmail.com>
2026-06-11 14:15:34 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1> Essa é da boa! </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <span className="navbar-logo"> Loja Da Boa </span>
+      <span className="navbar-bag"> Saquinho (0) </span>
+    </nav>
+  );
+}
+export default Navbar;
