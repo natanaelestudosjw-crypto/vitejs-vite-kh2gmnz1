f0bf71cf19be2c07200a38c99bf4617aae1dcc07
natanaelestudosjw-crypto <natanael.estudos.jw@gmail.com>
2026-06-12 08:48:31 -0300

    finalização da loja. faltam correções e estilo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ function Navbar({bagCount}) {
   return (
     <nav className="navbar">
       <span className="navbar-logo"> Biqueira Express </span>
-      <span className="navbar-bag"> Saquinho ({bagCount}) </span>
+      <span className="navbar-bag" onclick={onBagClick}>
+      Saquinho ({bagCount}) </span>
     </nav>
   );
 }
